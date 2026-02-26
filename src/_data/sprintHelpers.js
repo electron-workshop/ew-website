73aa4b48f3a9e4f0f8edc68065d1af83bb6d7280
starship-droid <github@sophieallen.space>
2026-02-27 10:01:08 +1100

    Sprint retro corrected to 6pm

--- src/_data/sprintHelpers.js
+++ src/_data/sprintHelpers.js
@@ -7,11 +7,11 @@ module.exports = function () {
   const { defaults, items } = sprintsData;
 
   // Normalise a session: can be a plain date string or an object with overrides
-  const normaliseSession = (session, sprint) => {
+  const normaliseSession = (session, sprint, overrideTime) => {
     const base = {
       online:      sprint.online      ?? defaults.online,
       physical:    sprint.physical    ?? defaults.physical,
-      time:        sprint.time        ?? defaults.time,
+      time:        overrideTime ?? sprint.time ?? defaults.time,
       physicalUrl: null,
     };
     if (typeof session === "string") return { ...base, date: session };
@@ -26,7 +26,7 @@ module.exports = function () {
     time:      sprint.time      ?? defaults.time,
     opening:   normaliseSession(sprint.opening   ?? sprint.startDate, sprint),
     midReview: normaliseSession(sprint.midReview ?? sprint.startDate, sprint),
-    retro:     normaliseSession(sprint.retro     ?? sprint.endDate,   sprint),
+    retro:     normaliseSession(sprint.retro     ?? sprint.endDate,   sprint, sprint.retroTime ?? defaults.retroTime),
   }));
 
   const current = sprints.find((s) => {
